bfc0c86bceed8bf78ec2b5e4ab1351f0114b23cb
digosts <rsantosdesenv@gmail.com>
2020-03-27 10:56:28 -0300

    Pasta publish

---
+++ ReactJs/publish/precache-manifest.243ecf3485503931e083016029aaa494.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa3b7367504a3571036b58050127c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44139e7688a3938fdbd6",
+    "url": "/static/css/main.0892ebba.chunk.css"
+  },
+  {
+    "revision": "593135854581b040fd0b",
+    "url": "/static/js/2.d294259e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d294259e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44139e7688a3938fdbd6",
+    "url": "/static/js/main.0ee5f781.chunk.js"
+  },
+  {
+    "revision": "c0832e453b97a1c23294",
+    "url": "/static/js/runtime-main.4eba2ab0.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
